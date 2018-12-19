338f6b00cd1e055e61e0261399afa3a6ef69b651
Felipe Hernandez Palafox <felipe.sistemas.fhp@gmail.com>
2018-12-19 01:53:29 -0600

    Curso de flutter
    Curso de flutter

---
+++ flutter_app2/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app2/src/app/App.dart';
+
+void main() => runApp(EstructuraApp());
+
---
+++ flutter_app_estructura/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_estructura/src/app/app.dart';
+
+//Estructura de la aplicación
+void main() => runApp(EstructuraApp());
